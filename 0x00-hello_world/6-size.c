a6d84e221cf62bbe3ba5a8246caee0e9a9952627
Raymond Omoraka <omorakaraymond2000@gmail.com>
2022-12-08 00:44:50 -0800

    no space allowed error fixed

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,7 +6,7 @@
  * Return: Always 0 (Success)
 */
 
-int main (void)
+int main(void)
 {
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
