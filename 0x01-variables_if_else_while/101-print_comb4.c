45f525631a0e1417fc9b5904f766d3487374a2d4
Raymond Omoraka <omorakaraymond2000@gmail.com>
2022-12-09 13:18:41 -0800

    second advance task fix

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,22 +8,26 @@
 
 int main(void)
 {
-	int a, b;
+	int a, b, c;
 
 	for (a = '0'; a < '9'; a++)
 	{
 		for (b = a + 1; b <= '9'; b++)
 		{
-			if (a != b)
+			for (c = b + 1; c <= '9'; c++)
 			{
-				putchar(a);
-				putchar(b);
+				if (a != b && b != c)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
 
-				if (a == '8' && b == '9')
-					continue;
+					if (a == '7' && b == '8')
+						continue;
 
-				putchar(',');
-				putchar(' ');
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
