f53357825da707d3e22140b3ce7279e68101b485
Raymond Omoraka <omorakaraymond2000@gmail.com>
2022-12-13 06:08:23 -0800

    advanced task 1 corrected

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,6 +4,7 @@
  * print_times_table - Prints the times table of the input,
  * @n: The value of the times table to be printed.
  */
+
 void print_times_table(int n)
 {
 	int i, j, prod;
@@ -16,35 +17,28 @@ void print_times_table(int n)
 
 			for (j = 1; j <= n; j++)
 			{
+				_putchar(',');
+				_putchar(' ');
 
 				prod = i * j;
 
+				if (prod <= 99)
+					_putchar(' ');
 				if (prod <= 9)
+					_putchar(' ');
+
+				if (prod >= 100)
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(prod % 10 + '0')
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10)) % 10 + '0');
 				}
-				if (prod > 9 && <= 99)
+				else if (prod <= 99 && prod >= 10)
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(prod / 10 + '0');
-					_putchar(prod % 10 + '0');
+					_putchar((prod / 10) + '0');
 				}
-				if (prod > 99)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(prod / 100 + '0');
-					_putchar(prod / 10 % 10 + '0');
-					_putchar(prod % 10 + '0');
-				}
+				_putchar((prod % 10) + '0');
 			}
-		_putchar('\n');
+			_putchar('\n');
 		}
 	}
 }
