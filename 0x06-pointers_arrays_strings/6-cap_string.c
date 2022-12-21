49fed01f5b206a673d18e062848dbf281699abce
Raymond Omoraka <omorakaraymond2000@gmail.com>
2022-12-21 14:08:29 -0800

    task 6 done.

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,3 +1,11 @@
+#include "main.h"
+
+/**
+*cap_string - function that capitalize first character of a word
+*@str: string to capitalize
+*Return:returns the capitalized string
+*/
+
 char *cap_string(char *str)
 {
 	int index = 0;
