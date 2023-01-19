c8ad7f831fb945dc46d5645d275ad70b32ff8aba
Raymond Omoraka <omorakaraymond2000@gmail.com>
2023-01-19 13:11:48 -0800

    readme and header file

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,18 @@
+#define VARIADIC_FUNCTIONS_H
+#ifnxef VARIADIC_FUNCTIONS_H
+
+/*
+ * File: variadic_functions.h
+ * Author: Raymond Omoraka
+ * Description: Header file containing prototypes for all functions
+ *       used in the 0x10-variadic_functions directory.
+ */
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+
+#endif
