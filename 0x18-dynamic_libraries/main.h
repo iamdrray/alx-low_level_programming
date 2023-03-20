a6678de48e680f90187c0479289917a17e88ddfe
Raymond Omoraka <omorakaraymond2000@gmail.com>
2023-03-20 14:20:25 -0700

    task 0

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,9 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-#include <stdio.h>
-#include <stdlib.h>
-
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -24,5 +20,9 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
-#endif /* MAIN_H */
+int mul(int a, int b);
+int add(int a, int b);
+int div(int a, int b);
+int sub(int a, int b);
+int mod(int a, int b);
+#endif
